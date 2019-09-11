83d3807a84dd218d7be5e2534f02222012f8df62
Stephen Abello <stephen.abello@combodo.com>
2019-09-11 10:15:24 +0200

    New login extension sample, a bit more complexe than the previous one

---
+++ en.dict.itop-login-custom-content.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ *  * Copyright (C) 2013-2019 Combodo SARL
+ *  *
+ *  * This file is part of iTop.
+ *  *
+ *  * iTop is free software; you can redistribute it and/or modify
+ *  * it under the terms of the GNU Affero General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * iTop is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU Affero General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  
+ */
+
+
+Dict::Add('EN US', 'English', 'English', array(
+	'UI:Login:AdditionalContent:What' => 'What is iTop ?',
+	'UI:Login:AdditionalContent:Personalize' => 'PERSONALIZE',
+	'UI:Login:AdditionalContent:Personalize+' => 'iTop is a fully customizable portal that adapts to your IT processes',
+	'UI:Login:AdditionalContent:Freedom' => 'FREEDOM',
+	'UI:Login:AdditionalContent:Freedom+' => 'iTop is an open source solution constantly enriched by an active community',
+	'UI:Login:AdditionalContent:Simplicity' => 'SIMPLICITY',
+	'UI:Login:AdditionalContent:Simplicity+' => 'A ready to use programmable tool that can be integrated into complex environments',
+	'UI:Login:AdditionalContent:Reactivity' => 'REACTIVITY',
+	'UI:Login:AdditionalContent:Reactivity+' => 'A web solution that gives the ability to respond better and faster  to user requests',
+));
---
+++ src/CustomContentLoginExtension.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ *
+ *  * Copyright (C) 2013-2019 Combodo SARL
+ *  *
+ *  * This file is part of iTop.
+ *  *
+ *  * iTop is free software; you can redistribute it and/or modify
+ *  * it under the terms of the GNU Affero General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * iTop is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU Affero General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  
+ */
+
+namespace Combodo\iTop\LoginCustomContent;
+
+use iLoginDataExtension;
+use LoginBlockData;
+use LoginTwigData;
+use utils;
+
+/**
+ * Class CustomContentLoginExtension
+ */
+class CustomContentLoginExtension implements  iLoginDataExtension
+{
+
+	/**
+	 * Return the list of supported login modes for this plugin
+	 *
+	 * @return array of supported login modes
+	 */
+	public function ListSupportedLoginModes()
+	{
+		return array('form','basic', 'external', 'url');
+	}
+
+	/**
+	 * @return LoginTwigData
+	 */
+	public function GetLoginData()
+	{
+		$sPath = APPROOT.'env-'.utils::GetCurrentEnvironment().'/itop-login-custom-content/view';
+		$oLoginData = new LoginTwigData(array(), $sPath);
+		$oLoginData->AddBlockData('body', new LoginBlockData('base.html.twig'));
+		$oLoginData->AddBlockData('login_additional_content', new LoginBlockData('additional_content.html.twig'));
+
+		$oLoginData->AddBlockData('css', new LoginBlockData('custom_css.css.twig'));
+
+		return $oLoginData;
+	}
+}
